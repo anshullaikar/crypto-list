7b8ac1e670c07080b67978100544b7686f35b1b7
anshul <anshullaikar@gmail.com>
2022-03-20 01:55:21 +0530

    added timestamp to API and designed the logic a bit

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
-const express = require("express"); //Line 1
+const express = require("express"); 
 var request = require("request");
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+const app = express(); 
+const port = process.env.PORT || 5000; 
 
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
 app.get("/express_backend", (req, res) => {
     res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Content-Type", "application/json");
+    res.status(200);
     var options = {
         method: "GET",
-        url: "https://api.coinranking.com/v2/coins?limit=100",
+        url: "https://api.coinranking.com/v2/coins?limit=1",
         headers: {
             "x-access-token":
                 "coinranking19759dda7c96a7c6de698bcf410c5fa15e5bb8840d065f46",
         },
     };
     request(options, function (error, response) {
-        if (error) res.send(error);
-        res.send(response.body);
+        if (error) {
+            res.status(400);
+            res.send(error);
+        }
+        let data = {};
+        try{
+        data = JSON.parse(response.body);
+        }
+        catch(err){
+            res.status(400);
+            res.send(err.message);
+        }
+        data = { ...data, timestamp: Math.floor(new Date().getTime() / 1000) };
+        res.send(data); //return timestamp for data as well
     });
-}); //Line 11
+}); 
